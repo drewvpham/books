bea4a5c3ec30caacace0eb5707d4f82ef68a22bc
Drew Pham <drewvpham@gmail.com>
2017-05-18 16:48:59 -0500

    solution

--- server.py
+++ server.py
@@ -1,53 +1,49 @@
 from flask import Flask, request, render_template, redirect, session, flash
 from mysqlconnection import MySQLConnector
 import re
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 app = Flask(__name__)
-mysql = MySQLConnector(app, 'email_validation')
+mysql = MySQLConnector(app, 'books')
 app.secret_key = "ThisIsSecret!"
 
 @app.route('/')
 def index():
-    emails = mysql.query_db("select id, email, date from emails")
+    books = mysql.query_db("select id, title, author, date_added from books")
+    return render_template('index.html', books=books)
 
-
-    return render_template('index.html', emails=emails)
-
-@app.route('/register', methods=["POST"])
-def register():
-
-    errors={}
-    if len(request.form['first_name']) > 2 or not request.form['first_name'].isalpha():
-        errors['first_name']='First Name must be at least 3 characters in length and all alphabetic characters.'
-        #flash("Email cannot be blank!")
-    if len(request.form['last_name']) > 2 or not request.form['last_name'].isalpha():
-        errors['last_name']='Last Name must be at least 3 characters in length and all alphabetic characters.'
-    if len(request.form['email']) is 0:
-        errors['email']'Email can not be empty'
-    if not EMAIL_REGEX.MATCH(request.form['email']):
-        errors['email']='Invalid email format.'
-    if len(request.form['password']) < 8:
-        errors['password']='Password must be at least 8 characters in length'
-    if request.form['password'] is not request.form['password_confirmation']:
-        errors['password']='Password and Password Confirmation do not match'
-
-    if errors:
-        flash(errors)
-
-
-
-    elif not EMAIL_REGEX.match(request.form['email']):
-        flash("Invalid Email Address!")
-    else:
-        flash("Success!")
-        result = mysql.query_db(query, data)
+@app.route('/add', methods=["POST"])
+def add():
+    query = "insert into books (title, author, date_added) values (:title, :author, now())"
+    print query
+    result = mysql.query_db(query, request.form)
+    print result
     return redirect('/')
 
+@app.route('/delete/<bookid>')
+def delete(bookid):
+    query = "DELETE FROM books WHERE id=:id"
+    data = {'id': bookid}
+    mysql.query_db(query, data)
+    return redirect('/')
 
+@app.route('/show/<bookid>')
+def show(bookid):
+    print 'here'
+    query = "select id, title, author, date_added from books where id=:id"
+    data = {
+        'id': bookid
+    }
+    book=mysql.query_db(query, data)[0]
+    return render_template('show.html', book=book)
 
-
-    print result
-
-
+@app.route('/update/<bookid>', methods=['POST'])
+def update(bookid):
+    query = "UPDATE books SET title = :title, author = :author WHERE id = :id"
+    data = {
+             'title': request.form['title'],
+             'author':  request.form['author'],
+             'id': bookid
+           }
+    changes=mysql.query_db(query, data)
+    return redirect('/')
 app.run(debug=True)
